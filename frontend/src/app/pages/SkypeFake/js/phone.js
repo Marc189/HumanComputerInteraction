3e0719b83ca35fec7065cb6a5846f0b837c3d7dc
Marc Niemella <csd4787@csd.uoc.gr>
2020-11-21 22:59:50 +0200

    remote

---
+++ frontend/src/app/pages/SkypeFake/js/phone.js
@@ -0,0 +1,10 @@
+function change(){
+  if (document.getElementById('switch').style.display ==='none'){
+    document.getElementById('switch').style.display = 'block';
+    document.getElementById('switch2').style.display = 'none';
+  }else {
+    document.getElementById('switch2').style.display = 'block' ;
+    document.getElementById('switch').style.display = 'none';
+  }
+
+}
