40d7b4719191efe72703ff5da5b81c14b89e46cc
Marc Niemella <csd4787@csd.uoc.gr>
2020-12-08 19:41:30 +0200

    "dynamic" and other small changes

--- frontend/src/app/pages/SkypeFake/js/phone.js
+++ frontend/src/app/pages/SkypeFake/js/phone.js
@@ -1,12 +1,40 @@
 var mic=false;
 var vid=false;
 
+var user=[["Muster80","1234","Max Mustermann","m_muster@gmail.com","1980-10-03"],
+  ["MF189","test","Marc Niemella","mfniemella@gmail.com","1999-09-18"],
+  ["Trav","test","Traves Stolterfoht","travesmarlon@gmail.com","1900-01-01"]];
+
+var part;
+
+
+// function loaduser(){
+//   $.ajax({
+//     url: 'user.csv',
+//     dataType: 'text',
+//   }).done(successFunction);
+// }
+//
+// function successFunction(data) {
+//   var allRows = data.split(/\r?\n|\r/);
+//   for (var singleRow = 0; singleRow < allRows.length-1; singleRow++) {
+//     var rowCells = allRows[singleRow].split(',');
+//     var us=[];
+//     for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+//       us.push(rowCells[rowCell]);
+//     }
+//     user.push(us)
+//   }
+// }
+
+
+
 function mic_vid(){
   if (vid){
     change2('vid');
     video();
   }
-  if(mic){
+  if(!mic){
     change2('mute');
     icon('muteIcon');
   }
@@ -14,16 +42,34 @@ function mic_vid(){
 function updateDiv()
 {
   $("#duringCall").load(" #duringCall > *");
+  $("#calling").load(" #calling > *");
 }
-function calling2(){
-    document.getElementById('home').style.display="none";
-    document.getElementById('calling').style.display="block";
-    setTimeout("next()", 5000);
 
-}
+
 function calling(){
   if(document.querySelectorAll("input:checked").length>0){
-    calling2();
+    part=document.querySelectorAll("input:checked");
+    var str;
+    if(part.length===1){
+      str="Calling "+part[0].value+"...";
+    }
+    else{
+      str="Calling Group.."
+      document.getElementById('pic').style.display="none";
+      for(var i=0;i<part.length;i++){
+        var div=document.createElement("div");
+        div.setAttribute("style","display: inline-block;height: 140px ;width: 130px")
+        div.innerHTML= "<img src=\"media/hiclipart.com.png\" style=\"height: 100px;display: inline-block \">";
+        var div2= document.createElement("div");
+        div2.innerText=part[i].value;
+        div.appendChild(div2);
+        document.getElementById('pict').appendChild(div);
+      }
+    }
+    document.getElementById("names").innerText=str;
+    document.getElementById('home').style.display="none";
+    document.getElementById('calling').style.display="block";
+    setTimeout("next()", 500);
     mic_vid();
   }
 }
@@ -32,6 +78,7 @@ function next(){
   if (document.getElementById('calling').style.display==="block"){
     document.getElementById('duringCall').style.display="block";
     document.getElementById('calling').style.display="none"
+    //document.getElementById('video').play();
   }
 }
 
@@ -57,16 +104,49 @@ function change(){
   }
 
 }
-function chat(){
+function chat(type){
   if(document.getElementById('videoElement').style.display==="inline-block"){
     video();
     vid=true;
   }
-  if (document.getElementById("buttons").style.display==='block') {
     document.getElementById('buttons').style.display = 'none';
-    document.getElementById('keyboard').style.display= 'block';
+    document.getElementById('vi').style.display="none";
+    document.getElementById('back').style.display= 'inline-block';
+    document.getElementById('chat2').style.display= 'block';
     document.getElementById('type').style.marginBottom='5px';
-  }
+    switch (type){
+      case "emoji":
+        document.getElementById('em').style.display= 'block';
+        document.getElementById('keyboard').style.display= 'none';
+        document.getElementById('dat').style.display= 'none';
+        break;
+      case "text":
+        document.getElementById('keyboard').style.display= 'block';
+        document.getElementById('em').style.display= 'none';
+        document.getElementById('dat').style.display= 'none';
+        break;
+      case "data":
+        document.getElementById('dat').style.display= 'block';
+        document.getElementById('keyboard').style.display= 'none';
+        document.getElementById('em').style.display= 'none';
+        break;
+    }
+    if(document.querySelectorAll("input:checked").length===1){
+      document.getElementById('group').style.display="none";
+      document.getElementById('name6').style.display= 'block';
+      document.getElementById('name5').innerText=part[0].value;
+    }
+    else{
+      document.getElementById('group').style.display="block";
+      document.getElementById('name6').style.display= 'none';
+    }
+}
+
+function chat2(name){
+  document.getElementById('parti').style.display= 'none';
+  document.getElementById('chat3').style.display= 'block';
+  document.getElementById('nam').innerText=name;
+
 }
 function change2(id){
   var butt=document.getElementById(id);
@@ -141,17 +221,21 @@ function change3(){
       main[i].style.margin= '20px 3px';
     }
     document.getElementById('hang').style.marginLeft='5px';
-    document.getElementById('video').style.display="block"
+    document.getElementById('vi').style.display="block";
+
   }else {
     document.getElementById('back').style.display="none";
     showCall();
   }
 }
 function back(){
-  document.getElementById('back').style.display="none"
-  if(document.getElementById('buttons').style.display==='none'){
+  if(document.getElementById('vi').style.display==='block'||document.getElementById('chat2').style.display=== 'block'){
+    document.getElementById('back').style.display="none";
     document.getElementById('buttons').style.display='block';
     document.getElementById('keyboard').style.display='none';
+    document.getElementById('em').style.display='none';
+    document.getElementById('dat').style.display='none';
+    document.getElementById('chat2').style.display= 'none';
     if(vid){
       video();
       vid=false;
@@ -159,15 +243,21 @@ function back(){
   }
   showCall();
   if (document.getElementById('participants').style.display==="block") {
-    document.getElementById('participants').style.display = "none";
-    document.getElementById('remote').style.display = "block";
+    if(document.getElementById('chat3').style.display=== 'block'){
+      document.getElementById('chat3').style.display= 'none';
+      document.getElementById('parti').style.display= 'block';
+    }else {
+      document.getElementById('back').style.display="none";
+      document.getElementById('participants').style.display = "none";
+      document.getElementById('remote').style.display = "block";
+    }
   }
 }
 
-function showCall(){
+function showCall() {
   var main = document.getElementsByClassName('buttonmain');
   var sec = document.getElementsByClassName('buttonsec');
-  if(sec[0].style.display==='none') {
+  if (sec[0].style.display === 'none') {
     document.getElementById('call').innerHTML = "Show Call"
     for (var i = 0; i < sec.length; i++) sec[i].style.display = 'inline-block';
     for (var i = 0; i < main.length; i++) {
@@ -175,174 +265,188 @@ function showCall(){
       main[i].style.width = '150px';
       main[i].style.fontSize = '24px';
       main[i].style.margin = '30px 14px';
+      document.getElementById('vi').style.display = "none";
     }
   }
 }
 
-function partis(){
-  document.getElementById('participants').style.display="block";
-  document.getElementById('remote').style.display="none";
-  document.getElementById('back').style.display="inline-block"
-}
+  function partis() {
+    document.getElementById('participants').style.display = "block";
+    document.getElementById('remote').style.display = "none";
+    document.getElementById('back').style.display = "inline-block"
+  }
 
 
+  function video() {
+    var vid = document.getElementById('videoElement');
+    var video = document.querySelector("#videoElement");
+    if (vid.style.display === 'none') {
+      if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({video: true})
+          .then(function (stream) {
+            video.srcObject = stream;
+          })
+          .catch(function () {
+            console.log("Something went wrong!");
+          });
+      }
+      vid.style.display = 'inline-block';
+      document.getElementById('type').style.width = "150px";
+      document.getElementById('type').style.height = "30px";
+      document.getElementById('type').style.marginTop = "60px"
+      document.getElementById('type').style.fontSize = "15px";
+      document.getElementById('emoji').style.width = "20px"
+      document.getElementById('data').style.width = "20px"
 
-function video(){
-  var vid=document.getElementById('videoElement');
-  var video = document.querySelector("#videoElement");
-  if(vid.style.display==='none') {
-    if (navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({video: true})
-        .then(function (stream) {
-          video.srcObject = stream;
-        })
-        .catch(function () {
-          console.log("Something went wrong!");
-        });
+    } else {
+      vid.style.display = 'none';
+      document.getElementById('type').style.width = "250px";
+      document.getElementById('type').style.height = "50px";
+      document.getElementById('type').style.fontSize = "20px";
+      document.getElementById('type').style.marginTop = "30px"
+      document.getElementById('emoji').style.width = "30px"
+      document.getElementById('data').style.width = "30px"
     }
-    vid.style.display = 'inline-block';
-    document.getElementById('type').style.width="150px";
-    document.getElementById('type').style.height="30px";
-    document.getElementById('type').style.marginTop="60px"
-    document.getElementById('type').style.fontSize="15px";
-    document.getElementById('emoji').style.width="20px"
-    document.getElementById('data').style.width="20px"
-
-  }else{
-    vid.style.display='none';
-    document.getElementById('type').style.width="250px";
-    document.getElementById('type').style.height="50px";
-    document.getElementById('type').style.fontSize="20px";
-    document.getElementById('type').style.marginTop="30px"
-    document.getElementById('emoji').style.width="30px"
-    document.getElementById('data').style.width="30px"
   }
-}
-function icon(id){
-  var ic=document.getElementById(id);
-  if(ic.style.display==='none'){
-    ic.style.display="inline-block";
 
-  }else{
-    ic.style.display='none';
-  }
-}
+  function icon(id) {
+    var ic = document.getElementById(id);
+    if (ic.style.display === 'none') {
+      ic.style.display = "inline-block";
 
-function showChecked() {
-  document.getElementById("result").textContent = document.querySelectorAll("input:checked").length +" Selected";
-}
-
-function  addContact(){
-  document.getElementById('add').style.display="inline-block";
-  document.getElementById('addContact').style.display="none";
-  document.getElementById("addName").style.display="block";
-  document.getElementById("addemail").style.display="block";
-
-}
-function add() {
-  var newName = document.getElementById("addName").value;
-  if(newName!=="") {
-    var nodeCheck = document.createElement("INPUT");
-    nodeCheck.type = "checkbox";
-    nodeCheck.setAttribute("class", "person");
-    var nodeLabel = document.createElement("LABEL");
-    var textnode = document.createTextNode(newName);
-    nodeCheck.appendChild(textnode);
-    nodeLabel.appendChild(textnode)
-    document.getElementById("checkbox").appendChild(nodeCheck);
-    document.getElementById("checkbox").appendChild(nodeLabel);
-  }
-  document.getElementById('add').style.display="none";
-  document.getElementById('addContact').style.display="inline-block";
-  document.getElementById("addName").style.display="none";
-  document.getElementById("addemail").style.display="none";
-}
-function settings(){
-  if(document.getElementById('sett').style.display==="block"){
-    document.getElementById('sett').style.display="none";
-    document.getElementById("start").style.display="block";
-  }else{
-    document.getElementById('sett').style.display = "block";
-    document.getElementById("start").style.display = "none";
-  }
-}
-
-function edit(id1,id2){
-  var field=document.getElementById(id1);
-  var input = document.getElementById(id2);
-  if (field.style.display==="none") {
-    var x=input.value;
-    field.innerText=x;
-    if(id1==="name1"){
-      document.getElementById('name3').innerText=x;
+    } else {
+      ic.style.display = 'none';
     }
-    field.style.display = "inline-block";
-    input.style.display = "none";
-    document.getElementById('set').style.display="block";
-    document.getElementById('keyboard3').style.display="none";
-    document.getElementById('line').style.marginBottom="20px";
-
-  }else {
-    input.value=field.innerText;
-    field.style.display = "none";
-    input.style.display = "inline-block";
-    document.getElementById('set').style.display="none";
-    document.getElementById('keyboard3').style.display="block";
-    document.getElementById('line').style.marginBottom="65px";
   }
-}
 
-function logIN(){
-  var user= document.getElementById('username').value;
-  var pwd= document.getElementById('pwd').value;
-  if(true) {
-    document.getElementById('login').style.display = "none";
-    document.getElementById('home').style.display = "block";
+  function showChecked() {
+    document.getElementById("result").textContent = document.querySelectorAll("input:checked").length + " Selected";
+  }
+
+  function addContact() {
+    document.getElementById('add').style.display = "inline-block";
+    document.getElementById('addContact').style.display = "none";
+    document.getElementById("addName").style.display = "block";
+    document.getElementById("addemail").style.display = "block";
+
+  }
+
+  function add() {
+    var newName = document.getElementById("addName").value;
+    if (newName !== "") {
+      var nodeCheck = document.createElement("INPUT");
+      nodeCheck.type = "checkbox";
+      nodeCheck.setAttribute("class", "person");
+      var nodeLabel = document.createElement("LABEL");
+      var textnode = document.createTextNode(newName);
+      nodeCheck.appendChild(textnode);
+      nodeLabel.appendChild(textnode)
+      document.getElementById("checkbox").appendChild(nodeCheck);
+      document.getElementById("checkbox").appendChild(nodeLabel);
+    }
+    document.getElementById('add').style.display = "none";
+    document.getElementById('addContact').style.display = "inline-block";
+    document.getElementById("addName").style.display = "none";
+    document.getElementById("addemail").style.display = "none";
+  }
+
+  function settings() {
+    if (document.getElementById('sett').style.display === "block") {
+      document.getElementById('sett').style.display = "none";
+      document.getElementById("start").style.display = "block";
+    } else {
+      document.getElementById('sett').style.display = "block";
+      document.getElementById("start").style.display = "none";
+    }
+  }
+
+  function edit(id1, id2) {
+    var field = document.getElementById(id1);
+    var input = document.getElementById(id2);
+    if (field.style.display === "none") {
+      var x = input.value;
+      field.innerText = x;
+      if (id1 === "name1") {
+        document.getElementById('name3').innerText = x;
+      }
+      field.style.display = "inline-block";
+      input.style.display = "none";
+      document.getElementById('set').style.display = "block";
+      document.getElementById('keyboard3').style.display = "none";
+      document.getElementById('line').style.marginBottom = "20px";
+
+    } else {
+      input.value = field.innerText;
+      field.style.display = "none";
+      input.style.display = "inline-block";
+      document.getElementById('set').style.display = "none";
+      document.getElementById('keyboard3').style.display = "block";
+      document.getElementById('line').style.marginBottom = "65px";
+    }
+  }
+
+  function logIN() {
+    var usr = document.getElementById('username').value;
+    var pwd = document.getElementById('pwd').value;
+    var pwd2;
+    for (var i=0;i<user.length;i++){
+        if(user[i][0]===usr){
+        pwd2=user[i][1];
+        document.getElementById('name3').innerText=user[i][2];
+        document.getElementById('name').innerText=user[i][2];
+        document.getElementById('uname').innerText=user[i][0];
+        document.getElementById('mail').innerText=user[i][3];
+        document.getElementById('date').value=user[i][4];
+      }
+    }
+    // if (pwd2===pwd) {
+    if (true) {
+      document.getElementById('login').style.display = "none";
+      document.getElementById('home').style.display = "block";
+      keyhide();
+    } else document.getElementById('error').innerText = "Password or Username wrong!";
+  }
+
+
+  function signin() {
+    document.getElementById("lo").style.display = "none";
+    document.getElementById("s").style.display = "none";
+    document.getElementById("back2").style.display = "block";
+    document.getElementById("sig").style.display = "block";
     keyhide();
-  } else document.getElementById('error').innerText="Password or Username wrong!";
-}
 
-
-function signin(){
-  document.getElementById("lo").style.display="none";
-  document.getElementById("s").style.display="none";
-  document.getElementById("back2").style.display="block";
-  document.getElementById("sig").style.display="block";
-  keyhide();
-
-}
-function sign(){
-  var uname=document.getElementById("username2").value;
-  var pwd=document.getElementById("pwd2").value;
-  var name=document.getElementById("name4").value;
-  var email=document.getElementById("email").value;
-  if(uname===""||pwd===""||name===""||email===""){
-    document.getElementById("error2").innerText="Something is missing!";
-  }else {
-
+  }
+function sign() {
+  var uname = document.getElementById("username2").value;
+  var pwd = document.getElementById("pwd2").value;
+  var name = document.getElementById("name4").value;
+  var email = document.getElementById("email").value;
+  var date = document.getElementById("date2").value;
+  if (uname === "" || pwd === "" || name === "" || email === "" || date === "") {
+    document.getElementById("error2").innerText = "Something is missing!";
+  } else {
+    user+=[uname,pwd,name,email,date];
     back3();
   }
 }
 
-function back3(){
-  document.getElementById("lo").style.display="block";
-  document.getElementById("s").style.display="block";
-  document.getElementById("back2").style.display="none";
-  document.getElementById("sig").style.display="none";
-  document.getElementById("error2").innerText="";
-  keyhide();
-}
+  function back3() {
+    document.getElementById("lo").style.display = "block";
+    document.getElementById("s").style.display = "block";
+    document.getElementById("back2").style.display = "none";
+    document.getElementById("sig").style.display = "none";
+    document.getElementById("error2").innerText = "";
+    keyhide();
+  }
 
-function keyshow(){
-  document.getElementById('keyboard2').style.display="block";
-  document.getElementById('lo').style.height="250px";
-}
+  function keyshow() {
+    document.getElementById('keyboard2').style.display = "block";
+    document.getElementById('lo').style.height = "250px";
+  }
 
-function keyhide(){
-  document.getElementById('keyboard2').style.display="none";
-  document.getElementById('lo').style.height="450px";
-}
-
-
+  function keyhide() {
+    document.getElementById('keyboard2').style.display = "none";
+    document.getElementById('lo').style.height = "450px";
+  }
 
 
