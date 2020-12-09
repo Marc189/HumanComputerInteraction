bd1b058031c5001d983fd079a5f6132ca8b906a3
Traves Stolterfoht <csd4788@csd.uoc.gr>
2020-12-09 18:56:35 +0100

    chat draft and some css

--- frontend/src/app/pages/SkypeFake/welcome.php
+++ frontend/src/app/pages/SkypeFake/welcome.php
@@ -1,8 +1,36 @@
-<html>
-<body>
+<script src="https://www.gstatic.com/firebasejs/8.1.2/firebase-app.js"></script>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+<script src="https://www.gstatic.com/firebasejs/8.1.2/firebase-database.js"></script>
 
-</body>
-</html>
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBq8U_Iss38guu-VrHPPui59i6vW8uE5q8",
+    authDomain: "chatbot-2bdfe.firebaseapp.com",
+    projectId: "chatbot-2bdfe",
+    storageBucket: "chatbot-2bdfe.appspot.com",
+    messagingSenderId: "1018016749004",
+    appId: "1:1018016749004:web:c5c22320c40ac31223f969"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var myName = prompt("Enter your name");
+  function sendMsg() {
+    var message = document.getElementById("message").value;
+    firebase.database().ref("messages").push().set({
+      "sender": myName,
+      "message": message
+    });
+    return false;
+  }
+</script>
+<form onsubmit="return sendMsg();">
+  <input id="message" placeholder="Enter massage">
+
+  <input type="submit">
+</form>
+
