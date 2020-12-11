f7e08fecd85fa77184f609cd94f63c0852c832dc
Traves Stolterfoht <csd4788@csd.uoc.gr>
2020-12-11 14:09:30 +0100

    change css

--- frontend/src/app/pages/SkypeFake/js/tv.js
+++ frontend/src/app/pages/SkypeFake/js/tv.js
@@ -1,17 +1,10 @@
 var numberParticipants;
-var counter = 0;
 function showChecked() {
   document.getElementById("result").textContent = document.querySelectorAll("input:checked").length +" Selected";
   numberParticipants=document.querySelectorAll("input:checked").length;
   console.log(numberParticipants);
-}
-
-function createNElemts(){
-  for(var i=0; i < numberParticipants; i++)
-  {
-    var videoBox = document.createElement("DIV");
-    videoBox.id  = 'videoBoxNr' + (++counter);
-    var someEle = document.getElementById("participantsBox");
-    someEle.appendChild();
+  if(numberParticipants===1){
+    document.getElementById("oneParticipant").style.display="block";
   }
 }
+
